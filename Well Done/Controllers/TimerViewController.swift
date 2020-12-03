2c7ac716c86ed867ead923d1a0f753f7f8693528
Alexander Ha <Ha.alex.h@gmail.com>
2020-12-03 12:26:17 -0800

    Add start button sound effect

--- Well Done/Controllers/TimerViewController.swift
+++ Well Done/Controllers/TimerViewController.swift
@@ -13,18 +13,6 @@ class TimerViewController: UIViewController {
     
     //MARK: - Properties
     
-    private var timer = Timer()
-    private var timeLeft: TimeInterval = 0
-    private var cookTime: Double!
-    private var endTime: Date?
-    
-    private var calculator = CookTimeCalculator()
-    private var isRunning: Bool? = false
-    
-    private let notificationCenter = UNUserNotificationCenter.current()
-    
-    private var audioPlayer: AVAudioPlayer!
-    
     private let startButton: CustomTimerButton = {
         let button = CustomTimerButton(withTitle: "START", withBackgroundColor: #colorLiteral(red: 0.9999362826, green: 0.9020040035, blue: 0.4274116755, alpha: 1), isHidden: false)
         button.setDimensions(height: 60, width: 150)
@@ -63,7 +51,7 @@ class TimerViewController: UIViewController {
     
     private let progressLayer: CustomShapeLayer = {
         let layer = CustomShapeLayer()
-        layer.strokeColor = #colorLiteral(red: 0.9999362826, green: 0.9020040035, blue: 0.4274116755, alpha: 1).cgColor
+        layer.strokeColor = #colorLiteral(red: 1, green: 0.4196327627, blue: 0.4195776284, alpha: 1).cgColor
         layer.zPosition = 1
         return layer
     }()
@@ -104,36 +92,41 @@ class TimerViewController: UIViewController {
         return animation
     }()
     
-    //MARK: - Lifecycle
+    private var timer = Timer()
+    private var timeLeft: TimeInterval = 0
+    private var cookTime: Double!
+    private var endTime: Date?
     
+    private var calculator = CookTimeCalculator()
+    private var isRunning: Bool? = false
+    
+    private let notificationCenter = UNUserNotificationCenter.current()
+    
+    private var audioPlayer: AVAudioPlayer!
+    private var soundEffect: AVAudioPlayer!
+    
+    //MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureUI()
-        
     }
-    
     //MARK: - Selectors
     
     @objc func handleStartPressed(button: UIButton) {
+        startTime()
         notificationAlert()
         isRunning?.toggle()
-        
-        view.layer.addSublayer(progressLayer)
-        progressAnimation.duration = timeLeft
-        progressLayer.add(progressAnimation, forKey: "stroke")
-        pulsingLayer.add(pulsingAnimation, forKey: "pulsing")
-        endTime = Date().addingTimeInterval(timeLeft)
-        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
-        
         startButton.isHidden = true
         resetButton.isHidden = false
-        
+        buttonSoundEffect()
         button.liftUp()
     }
     
     @objc func handleResetPressed(button: UIButton) {
         reset()
+        buttonSoundEffect()
         button.liftUp()
     }
     
@@ -153,9 +146,8 @@ class TimerViewController: UIViewController {
     //MARK: - Helpers
     
     private func configureUI() {
-        
         view.backgroundColor = #colorLiteral(red: 0.96853441, green: 1, blue: 0.9685121179, alpha: 1)
-        navigationItem.title = PROTEIN.doneness.capitalized
+        navigationItem.title = "\(PROTEIN.type.capitalized) | \(PROTEIN.method.capitalized) | \(PROTEIN.doneness.capitalized)"
         navigationItem.hidesBackButton = true
         
         let center = view.center
@@ -163,13 +155,11 @@ class TimerViewController: UIViewController {
         let circularPath = UIBezierPath(arcCenter: center, radius: 118, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
         
         progressLayer.path = circularPath.cgPath
-        
         trackLayer.path = circularPath.cgPath
         view.layer.addSublayer(trackLayer)
         
         pulsingLayer.path = pulsingPath.cgPath
         pulsingLayer.position = center
-        
         pulsingLayer.frame = view.bounds
         view.layer.addSublayer(pulsingLayer)
         
@@ -188,50 +178,25 @@ class TimerViewController: UIViewController {
         stack.anchor(top: timerLabel.bottomAnchor, paddingTop: 65)
     }
     
-    private func notificationAlert() {
-        let content = UNMutableNotificationContent()
-        content.title = "Food is ready!"
-        content.body = "Don't let it burn!"
-        content.categoryIdentifier = "alarm"
-        content.userInfo = ["customData" : "Well Done"]
-        content.sound = UNNotificationSound.default
-        
-        let interval = TimeInterval(cookTime)
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: interval, repeats: false)
-        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-        
-        notificationCenter.add(request)
+    private func startTime() {
+        view.layer.addSublayer(progressLayer)
+        progressAnimation.duration = timeLeft
+        progressLayer.add(progressAnimation, forKey: "stroke")
+        pulsingLayer.add(pulsingAnimation, forKey: "pulsing")
+        endTime = Date().addingTimeInterval(timeLeft)
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
     }
     
-    private func playSound() {
-        guard let url = Bundle.main.url(forResource: "marimba", withExtension: ".wav") else { return }
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-            audioPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-            guard let player = audioPlayer else { return }
-            player.play()
-        } catch let error {
-            print(error.localizedDescription)
-        }
-    }
-    
-    private func alertUser() {
-        if timeLeft > 0 {
-            timeLeft = endTime?.timeIntervalSinceNow ?? 0
-            timerLabel.text = timeLeft.time
-        } else {
-            timerLabel.text = "00:00"
-            playSound()
-            pulsingLayer.removeAnimation(forKey: "pulsing")
-            timer.invalidate()
-            
-            let ac = UIAlertController(title: "Food's Ready", message: "\nDon't overcook it!", preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "Okay", style: .default, handler: { [weak self] _ in
-                self?.audioPlayer?.stop()
-            }))
-            present(ac, animated: true)
-        }
+    private func reset() {
+        timer.invalidate()
+        timeLeft = cookTime
+        timerLabel.text = timeLeft.time
+        progressLayer.removeFromSuperlayer()
+        pulsingLayer.removeAnimation(forKey: "pulsing")
+        isRunning = false
+        resetButton.isHidden = true
+        startButton.isHidden = false
+        notificationCenter.removeAllPendingNotificationRequests()
     }
     
     private func exitTimer() {
@@ -248,15 +213,63 @@ class TimerViewController: UIViewController {
         notificationCenter.removeAllPendingNotificationRequests()
     }
     
-    private func reset() {
-        timer.invalidate()
-        timeLeft = cookTime
-        timerLabel.text = timeLeft.time
-        progressLayer.removeFromSuperlayer()
-        pulsingLayer.removeAnimation(forKey: "pulsing")
-        isRunning = false
-        resetButton.isHidden = true
-        startButton.isHidden = false
-        notificationCenter.removeAllPendingNotificationRequests()
+    private func notificationAlert() {
+        let content = UNMutableNotificationContent()
+        content.title = "Food is ready!"
+        content.body = "Don't let it burn!"
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["customData" : "Well Done"]
+        content.sound = UNNotificationSound.default
+        
+        let interval = TimeInterval(cookTime)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: interval, repeats: false)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        notificationCenter.add(request)
     }
+    
+    private func alertUser() {
+        if timeLeft > 0 {
+            timeLeft = endTime?.timeIntervalSinceNow ?? 0
+            timerLabel.text = timeLeft.time
+        } else {
+            timerLabel.text = "00:00"
+            playAlarm()
+            pulsingLayer.removeAnimation(forKey: "pulsing")
+            timer.invalidate()
+            
+            let ac = UIAlertController(title: "Food's Ready", message: "\nDon't overcook it!", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Okay", style: .default, handler: { [weak self] _ in
+                self?.audioPlayer?.stop()
+            }))
+            present(ac, animated: true)
+        }
+    }
+    
+    private func playAlarm() {
+        guard let url = Bundle.main.url(forResource: "marimba", withExtension: ".wav") else { return }
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            audioPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            guard let player = audioPlayer else { return }
+            player.play()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func buttonSoundEffect() {
+        guard let url = Bundle.main.url(forResource: "buttonTock", withExtension: ".mp3") else { return }
+        do {
+            soundEffect = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            soundEffect.play()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+   
+    
 }
